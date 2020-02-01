9ad5e048e291727b0f850143879417cd1f1853d9
Ludovic ROBINOT <lrobinot@gmail.com>
2020-02-01 12:25:29 +0100

    Define plaform for packages

--- check-updates.sh
+++ check-updates.sh
@@ -5,10 +5,11 @@
 for package in $(cat inventory | yq '.all.vars.packages' | yq -r 'keys[]')
 do
   repo=$(cat inventory | yq -r .all.vars.packages.$package.repo)
+  platform=$(cat inventory | yq -r .all.vars.packages.$package.platform)
   if [ $repo != 'null' ]
   then
     version=$(cat inventory | yq -r .all.vars.packages.$package.version)
-    latest_version=$(curl -Ls "https://api.github.com/repos/$repo/releases/latest" | jq -r '.assets[].browser_download_url' | grep amd64 | sed -e 's@^.*download/v@@' -e 's@/.*$@@' | head -1)
+    latest_version=$(curl -Ls "https://api.github.com/repos/$repo/releases/latest" | jq -r '.assets[].browser_download_url' | grep $platform | sed -e 's@^.*download/v@@' -e 's@/.*$@@' | head -1)
 
     if [ "$version" != "$latest_version" ]
     then
