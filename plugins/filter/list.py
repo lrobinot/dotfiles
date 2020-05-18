c26800c676bcf95a9ca8f207ee1879e3abadd17f
Ludovic ROBINOT <lrobinot@gmail.com>
2020-05-18 17:34:26 +0200

    More special application installation

--- plugins/filter/list.py
+++ plugins/filter/list.py
@@ -9,22 +9,49 @@ class FilterModule(object):
     def filters(self):
         return {
             # filter map
-            'list_insert': list_insert,
+            'list_insert_after': list_insert_after,
+            'list_insert_before': list_insert_before,
             'list_append': list_append
         }
 
-def list_insert(inStr, value, after):
+
+def list_insert_after(inStr, value, after):
     l = inStr.strip('][').split(', ')
+    length = len(l) 
+    for i in range(length):
+        if l[i] == "'" + value + "'":
+            del l[i]
+            break
     length = len(l) 
     for i in range(length):
         if l[i] == "'" + after + "'":
             l.insert(i + 1, "'" + value + "'")
             break
-
     return "[" + ", ".join(l) + "]"
+
+
+def list_insert_before(inStr, value, before):
+    l = inStr.strip('][').split(', ')
+    length = len(l) 
+    for i in range(length):
+        if l[i] == "'" + value + "'":
+            del l[i]
+            break
+    length = len(l) 
+    for i in range(length):
+        if l[i] == "'" + before + "'":
+            l.insert(i, "'" + value + "'")
+            break
+    return "[" + ", ".join(l) + "]"
+
 
 def list_append(inStr, value):
     l = inStr.strip('][').split(', ')
+    length = len(l) 
+    for i in range(length):
+        if l[i] == "'" + value + "'":
+            del l[i]
+            break
     l.append("'" + value + "'")
     return "[" + ", ".join(l) + "]"
 
@@ -33,17 +60,17 @@ if __name__ == "__main__":
     str = "['aaa', 'bbb', 'ccc', 'ddd']"
     print("str = " + str)
 
-    out = list_insert(str, 'eee', 'aaa')
-    print("list_insert(str, 'eee', 'aaa') = " + out)
+    out = list_insert_after(str, 'eee', 'aaa')
+    print("list_insert_after(str, 'eee', 'aaa') = " + out)
 
-    out = list_insert(str, 'fff', 'bbb')
-    print("list_insert(str, 'fff', 'bbb') = " + out)
+    out = list_insert_after(str, 'fff', 'bbb')
+    print("list_insert_after(str, 'fff', 'bbb') = " + out)
 
-    out = list_insert(str, 'ggg', 'ccc')
-    print("list_insert(str, 'ggg', 'ccc') = " + out)
+    out = list_insert_after(str, 'ggg', 'ccc')
+    print("list_insert_after(str, 'ggg', 'ccc') = " + out)
 
-    out = list_insert(str, 'hhh', 'ddd')
-    print("list_insert(str, 'hhh', 'ddd') = " + out)
+    out = list_insert_after(str, 'hhh', 'ddd')
+    print("list_insert_after(str, 'hhh', 'ddd') = " + out)
 
-    out = list_insert(str, 'iii')
-    print("list_insert(str, 'iii') = " + out)
+    out = list_append(str, 'iii')
+    print("list_append(str, 'iii') = " + out)
