f0d7dbdf0ab65b95182010241a4ce70e931533d4
Ludovic ROBINOT <lrobinot@gmail.com>
2020-05-22 09:10:39 +0200

    Update tilda tab colors

--- plugins/filter/rgb.py
+++ plugins/filter/rgb.py
@@ -9,23 +9,40 @@ class FilterModule(object):
     def filters(self):
         return {
             # filter map
-            'rgb': rgb_hex_to_dec
+            'rgb': rgb_hex_to_dec,
+            'rgba': rgba
         }
 
 
-def rgb_hex_to_dec(inStr, component):
+def rgb_hex_to_dec(hexColor, component):
+    if len(hexColor) != 7:
+        raise Exception("Passed hex color '%s', needs to be in #xxyyzz format." % hexColor)
+
     if component == 'r':
-        return int(inStr[1:3], 16) * 255
+        return int(hexColor[1:3], 16) * 255
     if component == 'g':
-        return int(inStr[3:5], 16) * 255
+        return int(hexColor[3:5], 16) * 255
     if component == 'b':
-        return int(inStr[5:7], 16) * 255
+        return int(hexColor[5:7], 16) * 255
+
+def rgba(hexColor, t):
+    if len(hexColor) != 7:
+        raise Exception("Passed hex color '%s', needs to be in #xxyyzz format." % hexColor)
+
+    r = int(hexColor[1:3], 16)
+    g = int(hexColor[3:5], 16)
+    b = int(hexColor[5:7], 16)
+
+    return "rgba({r:d}, {g:d}, {b:d}, {t:.2f})".format(r=r, g=g, b=b, t=t / 100.)
 
 if __name__ == "__main__":
-    str = "#002f3d"
-    print(str)
-    print(" -> ")
+    str = "#2a092a"
 
-    print(rgb_hex_to_dec("#002f3d", "r"))
-    print(rgb_hex_to_dec("#002f3d", "g"))
-    print(rgb_hex_to_dec("#002f3d", "b"))
+    print("rgb: {str} -> r={r}, g={g}, b={b}".format(
+        str=str,
+        r=rgb_hex_to_dec(str, "r"),
+        g=rgb_hex_to_dec(str, "g"),
+        b=rgb_hex_to_dec(str, "b")
+    ))
+
+    print("rgba(90): {str} -> {rgba}".format(str=str, rgba=rgba(str, 90)))
