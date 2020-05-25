fe4380d8e6007684c70031e9ec3305e0c6a03115
Ludovic ROBINOT <lrobinot@gmail.com>
2020-05-25 10:46:47 +0200

    Update version of protoc & k9s

--- check-updates.sh
+++ check-updates.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+set -e
 #set -x
 
-for package in $(cat inventory | yq '.all.vars.packages' | yq -r 'keys[]')
+for package in $(yq read --tojson inventory | jq '.all.vars.packages' | jq -r 'keys[]')
 do
-  repo=$(cat inventory | yq -r .all.vars.packages.$package.repo)
-  platform=$(cat inventory | yq -r .all.vars.packages.$package.platform)
+  repo=$(yq read --tojson inventory | jq -r .all.vars.packages.$package.repo)
+  platform=$(yq read --tojson inventory | jq -r .all.vars.packages.$package.platform)
   if [ $repo != 'null' ]
   then
-    version=$(cat inventory | yq -r .all.vars.packages.$package.version)
+    version=$(yq read --tojson inventory | jq -r .all.vars.packages.$package.version)
     latest_version=$(curl -Ls "https://api.github.com/repos/$repo/releases/latest" | jq -r '.assets[].browser_download_url' | grep $platform | sed -e 's@^.*download/v@@' -e 's@/.*$@@' | head -1)
 
     if [ "$version" != "$latest_version" ]
